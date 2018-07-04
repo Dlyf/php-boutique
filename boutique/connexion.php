a66c455965c1ebba7759ec01b1d6254d6c80c1a0
Dlyf974 <domilung@gmail.com>
2018-07-04 11:56:14 +0200

    add menu profil

--- boutique/connexion.php
+++ boutique/connexion.php
@@ -1,15 +1,46 @@
 <?php require_once('inc/init.inc.php');
 
 /*----------------- TRAITEMENTS PHP ------------*/
-if(isset($_GET['action'] &&
-if(isset($_GET['action'] ==
+if(isset($_GET['action']) && $_GET['action'] ==
 'deconnexion') {
-  unset($_SESSION['membre'])
+  unset($_SESSION['membre']);
+}
+
+if (internauteEstConnecte()) {
+  header('location:profil.php');
+  exit();
+}
+
+if ($_POST) {
+  $resultat = $pdo->prepare("SELECT * FROM membre WHERE pseudo = :pseudo");
+  $resultat->execute(array(':pseudo' => $_POST['pseudo']));
+  if($resultat->rowCount() >= 1) {
+    $membre = $resultat->fetch(PDO::FETCH_ASSOC);
+    if(password_verify($_POST['mdp'], $membre['mdp'])) {
+      $_SESSION['membre']['id_membre'] = $membre['id_membre'];
+      $_SESSION['membre']['pseudo'] = $membre['pseudo'];
+      $_SESSION['membre']['nom'] = $membre['nom'];
+      $_SESSION['membre']['prenom'] = $membre['prenom'];
+      $_SESSION['membre']['email'] = $membre['email'];
+      $_SESSION['membre']['civilite'] = $membre['civilite'];
+      $_SESSION['membre']['ville'] = $membre['ville'];
+      $_SESSION['membre']['code_postal'] = $membre['code_postal'];
+      $_SESSION['membre']['adresse'] = $membre['adresse'];
+      $_SESSION['membre']['statut'] = $membre['statut'];
+
+      header("location:profil.php");
+    } else {
+      $content .= '<div class="alert alert-danger" role="alert">Erreur de mot de passe</div>';
+    }
+  } else {
+    $content .= '<div class="alert alert-danger" role="alert">Erreur de pseudo</div>';
+  }
 }
  ?>
 
-/
+
 <?php require_once('inc/haut.inc.php'); ?>
+<?= $content; ?>
 <!-- Formulaire HTML de connexion
 - pseudo - input type="text"
 - mdp - input type="password"
--- boutique/inc/haut.inc.php
+++ boutique/inc/haut.inc.php
@@ -3,9 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Boutique</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
     <link rel="stylesheet" href="<?php echo URL; ?>../boutique/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
+  
   </head>
   <body>
     <header>
@@ -13,5 +16,40 @@
           <div>
               <a href="" title="Mon Sie">Monsite.com</a>
           </div>
+          <nav>
+            <ul>
+              <li><a href="<?= URL; ?>index.php">Accueil</a></li>
+              <?php if(internauteEstConnecteEtEstAdmin()): ?>
+              <li><a href="<?= URL; ?>admin/gestion-des-produits.php">Gestion des produits</a></li>
+              <?php endif; ?>
+              <?php if(internauteEstConnecte()): ?>
+              <li><a href="<?= URL; ?>panier.php">Panier</a></li>
+              <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Membre</button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <li>
+                        <ul>
+                            <li class="dropdown-item"><a href="<?= URL; ?>profil.php">Profil</a></li>
+                            <li class="dropdown-item">
+                                <a href="<?= URL; ?>connexion.php?action=deconnexion">Déconnexion</a></li>
+                        </ul>
+                    </li>
+                </div>
+                </div>
+                <?php else: ?>
+                <li><a href="<?= URL; ?>panier.php">Panier</a></li>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Membre</button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <li>
+                            <ul>
+                                <li class="dropdown-item"><a href="<?= URL; ?>inscription.php">Inscription</a></li>
+                                <li class="dropdown-item"><a href="<?= URL; ?>connexion.php">Connexion</a></li>
+                            </ul>
+                        </li>
+                    </div>
+                </div>
+                <?php endif; ?>
+                </nav>
     </header>
       <section class="container">
